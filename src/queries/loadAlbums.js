46492f2fead1871ea36086ab02e41174983bf071
sguiderk <samir.guiderk@gmail.com>
2019-04-21 13:31:43 +0200

    Correct the name of variable

--- src/queries/loadAlbums.js
+++ src/queries/loadAlbums.js
@@ -5,23 +5,19 @@ export default async (page, pageLength) => {
 	const albums = await fetch(conf.API_PHOTOS+`/albums`).then(response => response.json());
 	const albumsAmount = albums.length;
 
-	let star = 1;
+	let start = 1;
     if ( page > 1){
-		 star = page * pageLength - pageLength;
+		start = page * pageLength - pageLength;
 	}
 
 	const albumsQuery = Array(  page?pageLength:albumsAmount  )
 		.fill()
 		.map((_,position ) => {
-			position = position + star;
+			position = position + start;
 
 			let result =  fetch(conf.API_PHOTOS+`/albums/`+ position)
 				.then(response => response.json())
-				.then(json => ({
-					...json
-					,user:null
-				}))
-			    ;
+				.then(json => ({...json}));
 
 			return result;
 
--- src/queries/loadPhotos.js
+++ src/queries/loadPhotos.js
@@ -5,15 +5,15 @@ export default async (idAlbum ,page ,pageLength) => {
 	const photos = await fetch(conf.API_PHOTOS+`/photos/?albumId=`+idAlbum).then(response => response.json());
 	const photosAmount = photos.length;
 
-	let star = 1;
+	let start = 1;
 	if ( page > 1){
-		star = page * pageLength - pageLength;
+		start = page * pageLength - pageLength;
 	}
 
 	const photosQuery = Array(page?pageLength:photosAmount )
 		.fill()
 		.map((_, position) => {
-			position = position + star;
+			position = position + start;
 			return fetch(conf.API_PHOTOS+`/photos/`+ position )
 				.then(response => response.json())
 				.then(json => ({ ...json }));
